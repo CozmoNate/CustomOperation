e853ee7b7c8b86f05daef6865f61dc141e9acfb7
Natan Zalkin <natan.zalkin@me.com>
2019-12-02 20:47:19 +0200

    Add more sync options

--- CustomOperation/OperationQueue+AsyncOperation.swift
+++ CustomOperation/OperationQueue+AsyncOperation.swift
@@ -33,6 +33,8 @@ import Foundation
 
 extension OperationQueue {
 
+    /// Add opertaion to queue that reports finished when called provided completion block
+    /// - Parameter action: An action with completion
     @discardableResult
     public func addOperation(action: @escaping AsyncOperation.Action) -> Operation {
 
@@ -43,8 +45,10 @@ extension OperationQueue {
         return operation
     }
 
+    /// Add opertaion to queue and run action asynchronousely on provided DispatchQueue
+    /// - Parameter action: An action to execute
     @discardableResult
-    public func addOperation(queue: DispatchQueue, action: @escaping () -> Void) -> Operation {
+    public func addOperation(asyncOn queue: DispatchQueue, action: @escaping () -> Void) -> Operation {
 
         let operation = AsyncOperation { completion in
             queue.async {
@@ -58,11 +62,45 @@ extension OperationQueue {
         return operation
     }
     
+    /// Add opertaion to queue and run action asynchronousely on provided DispatchQueue.
+    /// Report operationfinished by calling provided completion block
+    /// - Parameter action: An action to execute
     @discardableResult
-    public func addOperation(queue: DispatchQueue, action: @escaping AsyncOperation.Action) -> Operation {
+    public func addOperation(asyncOn queue: DispatchQueue, action: @escaping AsyncOperation.Action) -> Operation {
 
         let operation = AsyncOperation { completion in
             queue.async { action(completion) }
+        }
+        addOperation(operation)
+
+        return operation
+    }
+    
+    /// Add opertaion to queue and run action synchronousely on provided DispatchQueue
+    /// - Parameter action: An action to execute
+    @discardableResult
+    public func addOperation(syncOn queue: DispatchQueue, action: @escaping () -> Void) -> Operation {
+
+        let operation = AsyncOperation { completion in
+            queue.sync {
+                action()
+                completion()
+            }
+        }
+        
+        addOperation(operation)
+
+        return operation
+    }
+    
+    /// Add opertaion to queue and run action synchronousely on provided DispatchQueue.
+    /// Report operationfinished by calling provided completion block
+    /// - Parameter action: An action to execute
+    @discardableResult
+    public func addOperation(syncOn queue: DispatchQueue, action: @escaping AsyncOperation.Action) -> Operation {
+
+        let operation = AsyncOperation { completion in
+            queue.sync { action(completion) }
         }
         addOperation(operation)
 
